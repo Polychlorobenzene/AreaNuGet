d64341b53a1f1173734698ee7de54292120a0d51
Marshall, Noel (ESD CONTR) <NMarshall@esd.wa.gov>
2018-06-22 14:33:40 -0700

    Install Contact NuGet Package created from Provider

---
+++ Consumer/Areas/Contact/Controllers/ContactController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Provider.Areas.Contact.Controllers
+{
+    public class ContactController : Controller
+    {
+        // GET: Contact/Contact
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your contact page from the package.";
+            return View();
+        }
+    }
+}
